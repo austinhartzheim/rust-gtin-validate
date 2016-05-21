b23b3b7075a3a03d072efcfe303ebfc631db82a3
Austin Hartzheim <austin@austinhartzheim.me>
2016-05-21 07:16:23 +0000

    Moved check-digit calculation to a separate function for unit testing. Also fixed the check-digit calculation.

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,42 @@
 
 use std::ascii::AsciiExt;
 
+
+/// Computes the check digit of a UPC-A code.
+/// Assumes that all bytes in `upc` are valid ASCII digits.
+fn compute_upca_check_digit(upc: &[u8]) -> u8 {
+    let mut even: u8 = 0;
+    let mut odd: u8 = 0;
+    let mut check: u8;
+    let mut curr: u8;
+    
+    for i in 0..11 {
+        curr = upc[i] - 48;
+        
+        if i % 2 == 0 {
+            odd += curr;
+        } else {
+            even += curr;
+        }
+    }
+
+    println!("even: {}\todd: {}", even, odd);
+    
+    //    check = (10 - (((3*odd + even) as i16) % 10)) as u8;
+    check = (3*odd + even) % 10;
+    if check > 0 {
+        check = 10 - check;
+    }
+    println!("Check digit: {}", check);
+    return check;
+}
+
 /// Check that a UPC-A code is valid by confirming that:
 /// * It is made of 12 digits
 /// * The check-digit is correct
 fn check_upca(upc: &str) -> bool {
-    let mut even: u8 = 0;
-    let mut odd: u8 = 0;
-    let check: i16;
-    let mut curr: u8;
-    
+    let check: u8;
+
     // Check that input is ASCII with length 12
     if upc.is_ascii() == false {
         return false;
@@ -22,28 +49,20 @@ fn check_upca(upc: &str) -> bool {
     }
 
     let bytes = upc.as_bytes();
-
-    // Calculate and compare check digit 
-    for i in 0..10 {
-        curr = bytes[i];
-
-        // Handle ASCII range for numbers. Convert to u8.
-        if curr < 48 || curr > 48 + 9 {
+    for i in 0 .. 12 {
+        // Checking that all bytes are ASCII digits
+        if bytes[i] < 48 || bytes[i] > 48 + 9 {
             return false;
         }
-        curr -= 48;
-        
-        if i % 2 == 0 {
-            even += curr;
-        } else {
-            odd += curr;
-        }
     }
+    
+    check = compute_upca_check_digit(bytes);
+
+    // Calculate and compare check digit 
     if bytes[11] < 48 || bytes[11] > 48 + 9 {
         return false;
     }
-    check = -1 * ((odd + even) as i16) % 10;
-    if check != ((bytes[11] - 48) as i16) {
+    if check != bytes[11] - 48 {
         return false;
     }
     
@@ -74,8 +93,17 @@ fn fix_upca(upc: &str) -> String {
 
 #[cfg(test)]
 mod tests {
+    use super::compute_upca_check_digit;
     use super::check_upca;
     use super::fix_upca;
+
+    #[test]
+    fn compute_upca_check_digit_static_data() {
+        assert_eq!(compute_upca_check_digit(&"000000000000".as_bytes()), 0);
+        assert_eq!(compute_upca_check_digit(&"123456789012".as_bytes()), 2);
+        assert_eq!(compute_upca_check_digit(&"123456789081".as_bytes()), 1);
+        assert_eq!(compute_upca_check_digit(&"036000291452".as_bytes()), 2);
+    }
     
     #[test]
     fn check_upca_valid() {
@@ -94,15 +122,22 @@ mod tests {
 
     #[test]
     fn check_upca_invalid_check_digit() {
-        assert!(check_upca(&"000000000001") == false);
-        assert!(check_upca(&"000000000002") == false);
-        assert!(check_upca(&"000000000003") == false);
-        assert!(check_upca(&"000000000004") == false);
-        assert!(check_upca(&"000000000005") == false);
-        assert!(check_upca(&"000000000006") == false);
-        assert!(check_upca(&"000000000007") == false);
-        assert!(check_upca(&"000000000008") == false);
-        assert!(check_upca(&"000000000009") == false);
+        assert_eq!(check_upca(&"000000000000"), true);
+        assert_eq!(check_upca(&"000000000001"), false);
+        assert_eq!(check_upca(&"000000000002"), false);
+        assert_eq!(check_upca(&"000000000003"), false);
+        assert_eq!(check_upca(&"000000000004"), false);
+        assert_eq!(check_upca(&"000000000005"), false);
+        assert_eq!(check_upca(&"000000000006"), false);
+        assert_eq!(check_upca(&"000000000007"), false);
+        assert_eq!(check_upca(&"000000000008"), false);
+        assert_eq!(check_upca(&"000000000009"), false);
+    }
+
+    #[test]
+    fn check_upca_static_data() {
+        assert_eq!(check_upca(&"123456789012"), true);
+        assert_eq!(check_upca(&"123456789013"), false);
     }
 
 
