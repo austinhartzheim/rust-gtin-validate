19569a3ffdebf3a33d03ca2c3b94aa76f3f5d7e3
Austin Hartzheim <austin@austinhartzheim.me>
2016-06-11 16:55:10 +0000

    Added documentation to lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! Validate GTIN-style barcodes, including the commonly-used UPC-A and
+//! EAN-13 codes.
+//! 
+//! In addition to validating the correctness of the codes, you can
+//! attempt to automatically repair the most common errors, such as the
+//! removal of leading zeroes by spreadsheet software (because the codes
+//! are sometimes treated as integers) and the removal of whitespace
+//! that can be accidentally introduced during data entry or spreadsheet
+//! conversion.
+//! 
+//! This crate supports the following code types:
+//! 
+//! * GTIN-8
+//! * GTIN-12 (UPC-A)
+//! * GTIN-13 (EAN-13)
+//! * GTIN-14
+
+
+
 // private modules for internal use
 mod utils;
 
